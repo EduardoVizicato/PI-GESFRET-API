6266cb637338e8b284c391c110abe58b80d9b018
Eduardo Vizicato <eduardovizicato@gmail.com>
2025-06-02 19:19:40 -0300

    commit

---
+++ TMS.Domain/ValueObjects/NameVO.cs
@@ -0,0 +1,23 @@
+﻿namespace TMS.Domain.ValueObjects;
+
+public class NameVO
+{
+    public NameVO(string name)
+    {
+        if(!IsValidFormat(name))
+            throw new ArgumentException("Invalid name");
+
+        Name = name;
+    }
+
+    public string Value => Name;
+    public string Name { get; }
+
+    public static bool IsValidFormat(string validName)
+    {
+        if(validName.Length < 2)
+            return false;
+
+        return true;
+    }
+}
