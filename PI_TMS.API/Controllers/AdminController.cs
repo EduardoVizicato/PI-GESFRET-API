c778ace490209454ed759465da2c92a7decc92f7
Eduardo Vizicato <eduardovizicato@gmail.com>
2025-06-16 21:57:01 -0300

    migrations

---
+++ PI_TMS.API/Controllers/AdminController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PI_TMS.API.Controllers;
+[Route("api/admin")]
+[ApiController]
+public class AdminController : Controller
+{
+    // I WILL DO IT LATER
+}
