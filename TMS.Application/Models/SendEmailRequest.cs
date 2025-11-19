58c73ce34d735720dfacb1465e6eff00ef26b514
FelipeBarbisan766 <felipebarbisan76@gmail.com>
2025-11-19 10:14:29 -0300

    Create Smtp Connection

---
+++ TMS.Application/Models/SendEmailRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Application.Models
+{
+    public record SendEmailRequest(string Recipient, string Subject, string Body);
+
+}
